13a3bf90c3001040678f430608fd17674aa052e9
AJ Jalusi <ajalusi26@gmail.com>
2022-01-10 16:28:20 -0500

    Done

--- src/Article.js
+++ src/Article.js
@@ -1,9 +1,17 @@
 import React from "react";
 
-function Article() {
+const Article = () => <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
+
+const Comment = () => <div>Naturally, I agree with this article.</div>;
+
+
+function App(){
   return (
-    <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
-  );
+    <div>
+      <Article />
+      <Comment />
+    </div>
+  )
 }
 
-export default Article;
+export default App;
